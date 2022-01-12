0798d2413c66f939288937583bc2cec03327804c
Nils Gu <info@nilsgumpfer.com>
2022-01-12 13:17:48 +0100

    2022-01-12_13:17:48

--- cnns/grad_simple.py
+++ cnns/grad_simple.py
@@ -1,60 +1,70 @@
-import math
-import os
-from math import tanh
-
-import imageio as imageio
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
+from matplotlib.colors import Normalize
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-from sklearn.metrics import mean_squared_error
-from matplotlib import cm
 
 
-def plot_function_surface(X, Y, Z, C, t, cmap='gist_earth'):
+def plot_function_surface(x1, x2, Z, C, t, cmap='gist_earth'):
     fig = plt.figure(figsize=(18, 12))
     ax = fig.add_subplot(projection='3d')
-    scamap = plt.cm.ScalarMappable(cmap=cmap)
-    ax.plot_surface(X, Y, Z, facecolors=scamap.to_rgba(C), cmap=cmap)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
+    vmax = np.max(np.abs(np.ravel(C)))
+    vmin = -vmax
+    norm = Normalize(vmin, vmax)
+    scamap = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    ax.plot_surface(x1, x2, Z, facecolors=scamap.to_rgba(C), cmap=cmap)
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
     ax.set_zlabel('z')
     ax.set_title(t)
     fig.colorbar(scamap, shrink=0.5, aspect=10)
     plt.tight_layout()
+    plt.savefig('{}.png'.format(t))
+    plt.close()
 
 
-def f(x, y):
-    # return y * np.sin(x ** 2)
-    return (x**2) * y
+def f(x1, x2):
+    return x2 * np.sin(x1 ** 2)
+    # return (x1**2) * x2
 
 
-def df_x(x, y):
-    # return 2 * x * y * np.cos(x ** 2)
-    return 2 * x * y
+def df_x1(x1, x2):
+    return 2 * x1 * x2 * np.cos(x1 ** 2)
+    # return 2 * x1 * x2
 
 
-def df_y(x, y):
-    # return np.sin(x ** 2)
-    return x ** 2
-
+def df_x2(x1, x2):
+    return np.sin(x1 ** 2)
+    # return x1 ** 2
 
 def main():
-    x = np.linspace(-2, 2, 50)
-    y = np.linspace(-5, 5, 50)
-    x, y = np.meshgrid(x, y)
+    x1 = np.linspace(-2, 2, 50)
+    x2 = np.linspace(-5, 5, 50)
+    x1, x2 = np.meshgrid(x1, x2)
 
-    plot_function_surface(X=x, Y=y, Z=f(x, y), C=f(x, y), t='Z=f(x, y), C=f(x, y)')
-    plot_function_surface(X=x, Y=y, Z=f(x, y), C=df_x(x, y), t='Z=f(x, y), C=df_x(x, y)')
-    plot_function_surface(X=x, Y=y, Z=f(x, y), C=df_y(x, y), t='Z=f(x, y), C=df_y(x, y)')
+    # Function plot
+    plot_function_surface(x1=x1, x2=x2, Z=f(x1, x2), C=f(x1, x2), t='Z=f(x1, x2), C=f(x1, x2)')
 
-    # plot_function_surface(X=x, Y=y, Z=df_x(x, y), C=f(x, y), t='Z=df_x(x, y), C=f(x, y)')
-    # plot_function_surface(X=x, Y=y, Z=df_x(x, y), C=df_x(x, y), t='Z=df_x(x, y), C=df_x(x, y)')
-    # plot_function_surface(X=x, Y=y, Z=df_x(x, y), C=df_y(x, y), t='Z=df_x(x, y), C=df_y(x, y)')
+    # Function plots with x1 and x2 gradients
+    plot_function_surface(x1=x1, x2=x2, Z=f(x1, x2), C=df_x1(x1, x2), t='Z=f(x1, x2), C=df_x1(x1, x2)')
+    plot_function_surface(x1=x1, x2=x2, Z=f(x1, x2), C=df_x2(x1, x2), t='Z=f(x1, x2), C=df_x2(x1, x2)')
 
-    # plot_function_surface(X=x, Y=y, Z=df_y(x, y), C=f(x, y), t='Z=df_y(x, y), C=f(x, y)')
-    # plot_function_surface(X=x, Y=y, Z=df_y(x, y), C=df_x(x, y), t='Z=df_y(x, y), C=df_x(x, y)')
-    # plot_function_surface(X=x, Y=y, Z=df_y(x, y), C=df_y(x, y), t='Z=df_y(x, y), C=df_y(x, y)')
+    # Function plots with x1 gradient * input and x2 gradient * input
+    plot_function_surface(x1=x1, x2=x2, Z=f(x1, x2), C=df_x1(x1, x2) * x1, t='Z=f(x1, x2), C=df_x1(x1, x2) * x1')
+    plot_function_surface(x1=x1, x2=x2, Z=f(x1, x2), C=df_x2(x1, x2) * x2, t='Z=f(x1, x2), C=df_x2(x1, x2) * x2')
 
-    plt.show()
+    # Gradient plots
+    plot_function_surface(x1=x1, x2=x2, Z=df_x1(x1, x2), C=df_x1(x1, x2), t='Z=df_x1(x1, x2), C=df_x1(x1, x2)')
+    plot_function_surface(x1=x1, x2=x2, Z=df_x2(x1, x2), C=df_x2(x1, x2), t='Z=df_x2(x1, x2), C=df_x2(x1, x2)')
+
+    # Gradient * input plots
+    plot_function_surface(x1=x1, x2=x2, Z=df_x1(x1, x2) * x1, C=df_x1(x1, x2) * x1, t='Z=df_x1(x1, x2) * x1, C=df_x1(x1, x2) * x1')
+    plot_function_surface(x1=x1, x2=x2, Z=df_x2(x1, x2) * x2, C=df_x2(x1, x2) * x2, t='Z=df_x2(x1, x2) * x2, C=df_x2(x1, x2) * x2')
+
+    # Gradient plots with gradient * input
+    plot_function_surface(x1=x1, x2=x2, Z=df_x1(x1, x2), C=df_x1(x1, x2) * x1, t='Z=df_x1(x1, x2), C=df_x1(x1, x2) * x1')
+    plot_function_surface(x1=x1, x2=x2, Z=df_x2(x1, x2), C=df_x2(x1, x2) * x2, t='Z=df_x2(x1, x2), C=df_x2(x1, x2) * x2')
+
+    # plt.show()
 
 main()
