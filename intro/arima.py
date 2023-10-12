8f8eca7804ad2c67653ff787b9a1fa265107d463
Nils Gu <info@nilsgumpfer.com>
2023-10-12 12:14:36 +0200

    2023-10-12_12:14:36

--- intro/arima.py
+++ intro/arima.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.arima.model import ARIMA
 
 
 df = pd.read_csv('../data/airline-passengers.csv')
-# values = df['Passengers'].values
-# model = ARIMA(values, order=(1, 1, 1), seasonal_order=(1, 1, 1, 12))
-# res = model.fit()
-# p = res.predict()
+values = df['Passengers'].values
+model = ARIMA(values[:50], order=(1, 1, 1), seasonal_order=(1, 1, 1, 2))
+res = model.fit()
+p = res.predict(start=0, end=144)
+plt.plot(p)
+plt.plot(values)
+plt.show()
--- intro/data.py
+++ intro/data.py
@@ -1,8 +1,4 @@
 import pandas as pd
-from matplotlib import pyplot as plt
 
-df = pd.read_csv('../data/airline-passengers.csv', index_col=0, parse_dates=True)
 
-values = df['Passengers'].values
-plt.plot(values, 'r--')
-plt.show()
+df = pd.read_csv('../data/airline-passengers.csv')
