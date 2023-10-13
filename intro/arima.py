8f406cf3ca94d118b71c89381606e110742d9c0c
Nils Gu <info@nilsgumpfer.com>
2023-10-13 13:07:41 +0200

    2023-10-13_13:07:41

--- intro/arima.py
+++ intro/arima.py
@@ -3,11 +3,20 @@ from matplotlib import pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 
 
+# Data loading
 df = pd.read_csv('../data/airline-passengers.csv')
 values = df['Passengers'].values
+
+# Model definition
 model = ARIMA(values[:50], order=(1, 1, 1), seasonal_order=(1, 1, 1, 2))
+
+# Model training
 res = model.fit()
+
+# Model usage
 p = res.predict(start=0, end=144)
+
+# Plot
 plt.plot(p)
 plt.plot(values)
 plt.show()
--- intro/data.py
+++ intro/data.py
@@ -1,4 +1,17 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
 df = pd.read_csv('../data/airline-passengers.csv')
+
+print(df)
+
+months = df['Month'].values
+passengers = df['Passengers'].values
+
+print(months)
+print(passengers)
+
+plt.plot(months, passengers)
+plt.xticks([months[0], months[-1]])
+plt.show()
