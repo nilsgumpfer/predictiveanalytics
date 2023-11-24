0bacc1bf5101df41016a8273d883df8d60bf61f6
Nils Gu <info@nilsgumpfer.com>
2023-11-24 09:41:52 +0100

    2023-11-24_09:41:52

--- cnns/sbx.py
+++
@@ -1,6 +0,0 @@
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras import Sequential
-from tensorflow.keras.activations import sigmoid
-import tensorflow as tf
-
-print(tf.__version__)
